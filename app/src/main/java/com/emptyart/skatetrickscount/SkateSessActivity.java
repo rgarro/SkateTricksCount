9fbf975297618ab12b78ddf6da7da2182c6f1763
Rolando <rgarro@gmail.com>
2026-09-24 11:00:20 -0600

    brave = new World(epsilon bliss) ...

--- app/src/main/java/com/emptyart/skatetrickscount/SkateSessActivity.java
+++ app/src/main/java/com/emptyart/skatetrickscount/SkateSessActivity.java
@@ -1,9 +1,14 @@
 package com.emptyart.skatetrickscount;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Spinner;
+import android.util.Log;
 
-public class SkateSessActivity extends AppCompatActivity {
+public class SkateSessActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
