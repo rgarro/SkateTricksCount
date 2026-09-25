8bfe14be7bf7326dd71d585c0e66f6fef699b4ab
Rolando <rgarro@gmail.com>
2026-09-25 10:15:43 -0600

    where were you whe I fell from grace a frozen heart an empty space

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # SkateTricksCount
+
+Plain text tricks counter app
+the ollies are supposed to land on the next number
+increasing the skater intelligence if they figure it out before
+
+This wont help on counties were skateboarding is forbiden
+
+
+Help me to buy my OneWheel https://onewheel.com/
+
+
+OneWheel streams all its info like the ODBII of the automotives
+so you can create your OneWheel mashup. 
+
+
+For Aliens is better to have a OneWheel than an E-Bike because is
+very simple to refactor and maintain until you get a place with a garage,
+like a pocket car.
+
+
+Gato my friend was cheap to buy a wetsuit and freeze to death in Washington surfing barrels
+
+God rest his soul
+
+
+With Ukraine We Stand !!!
--- app/src/main/java/com/emptyart/skatetrickscount/SkateSessActivity.java
+++ app/src/main/java/com/emptyart/skatetrickscount/SkateSessActivity.java
@@ -7,12 +7,42 @@ import android.view.View;
 import android.content.Intent;
 import android.widget.Spinner;
 import android.util.Log;
-
+/**
+ *            |    |    |
+ *           )_)  )_)  )_)
+ *          )___))___))___)\
+ *         )____)____)_____)\\
+ *      _____|____|____|____\\\__
+ * -------\                   /---------
+ *     ^^^^^ ^^^^^^^^^^^^^^^^^^^^^
+ *     ^^^^      ^^^^     ^^^    ^^
+ *           ^^^^      ^^^
+ * Sir Henry Morgan is de Lord of Talamanca
+ * Who bared record of the word of god, and of the
+ * testimony of Jesus Christ and of all things that he saw
+ *
+ *
+ *
+ * @author Rolando <rgarro@gmail.com>
+ */
 public class SkateSessActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skate_sess);
+        //choosing the trick
+        Button btnNext = (Button) findViewById(R.id.my_button);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Spinner spinner = (Spinner) findViewById(R.id.my_spinner);
+                String spinnerValue = spinner.getSelectedItem().toString();
+                Log.d("skate_trick", spinnerValue);
+                //Intent intent = new Intent(MainActivity.this, OllieLanding.class);
+                //intent.putExtra("beach_code", spinnerValue);
+                //startActivity(intent);
+            }
+        });
     }
 }
